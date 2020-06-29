9f2f52a3cb1916763e39985dc4f56cbdc9cf944f
Nicholas Ferry <nferry@email.sc.edu>
2020-06-28 20:58:43 -0400

    added notes about virtual attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # starting below we create the virtual attribute for first and last name with validation filters for the
+  # whole name entry (only one box for user to enter full name -- we take care of rest i.e. splitting name)
   validates_presence_of :name
 
   def first_name
     self.name.split.first
+    # rubified version -- python or other languages would index using [] (ex: self.name.split[0])
   end
 
   def last_name
+    self.name.split.last
   end
 
 end
