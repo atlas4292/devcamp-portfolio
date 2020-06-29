dc992b3ba959870d94b45f259810fc979ddf1f19
Nicholas Ferry <nferry@email.sc.edu>
2020-06-29 14:48:18 -0400

    Added guest user for current_user

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,17 @@
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+
+  # This concern separates a guest user from a user who is logged in where super = logged in
+  def current_user
+    super || guest_user
+  end
+
+  def guest_user
+    super || OpenStruct.new(name: "Guest User",
+                            first_name: "Guest",
+                            last_name: "User",
+                            email: "guest@somewhere.com"
+    )
+  end
+
+end
